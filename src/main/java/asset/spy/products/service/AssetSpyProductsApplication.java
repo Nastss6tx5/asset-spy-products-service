3d5c7b92ff7c1e324dabbd119ab2deb28e516f14
Nastss6tx5 <Nastasiia227@gmail.com>
2025-07-23 13:22:09 +0300

    feat(STOCK-99) fixed objectMapper

--- src/main/java/asset/spy/products/service/AssetSpyProductsApplication.java
+++ src/main/java/asset/spy/products/service/AssetSpyProductsApplication.java
@@ -1,11 +1,8 @@
 package asset.spy.products.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"asset.spy.products.service", "asset.spy.auth.lib"},
         exclude = org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration.class)
@@ -14,12 +11,5 @@ public class AssetSpyProductsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssetSpyProductsApplication.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper (){
-        return new ObjectMapper()
-                .findAndRegisterModules()
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
